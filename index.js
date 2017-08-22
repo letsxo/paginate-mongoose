6b77e3e9be2f56a61d09111bdff81404a4bd851d
Mate GABRI <mate@gabri.hu>
2017-08-22 21:03:38 +0930

    use object to pass in options; rename sort to order

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
 var Promise     = require('bluebird');
 var mongoPaging = Promise.promisifyAll(require('mongo-cursor-pagination'));
+var _           = require('lodash');
+
+var defaults = {
+  paginatedField: '_id',
+  projection: {
+    _id: 1
+  },
+  limit: 10,
+  order: 'asc'
+};
 
 module.exports = function paginatePlugin(schema, options) {
 
-  schema.static('findWithPagination', function(req, query, paginatedField, projection, limit, sort) {
+  schema.static('findWithPagination', function(req, query, queryOptions) {
+    queryOptions = _.merge({}, defaults, queryOptions);
+
+    if (queryOptions.order === 'desc') {
+      queryOptions.order = false;
+    } else {
+      queryOptions.order = true;
+    }
+
     return mongoPaging.findWithReqAsync(
       req,
       this.collection,
       {
         query: query || {},
-        paginatedField: paginatedField || '_id',
-        fields: projection || { _id: 1 },
-        limit: limit || 10,
-        sortAscending: sort || false
+        paginatedField: queryOptions.paginatedField,
+        fields: queryOptions.projection,
+        limit: queryOptions.limit,
+        sortAscending: queryOptions.order,
       }
     );
   });
